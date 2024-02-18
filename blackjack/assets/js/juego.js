ad349119d5564f4c5cf9196e325986d648f6cb39
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-18 16:28:03 -0700

    Se importó la librería de underscore.js y se creó la función de crearDeck()

--- blackjack/assets/js/juego.js
+++ blackjack/assets/js/juego.js
@@ -1,1 +1,29 @@
-console.log('Juego.js');
+/**
+ * 2C = Two of Clubs
+ * 2D = Two of Diamonts
+ * 2H = Two of Hearts
+ * 2S = Two of Spades
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+    for(let i = 2; i <= 10; i++) {
+        for(let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for(let tipo of tipos) {
+        for(let esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+    return deck;
+}
+
+crearDeck();
