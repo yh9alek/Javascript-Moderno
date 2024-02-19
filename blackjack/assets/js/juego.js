cde3cf1f589fe916f9dda17dd6ef4999e5207b7e
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-19 10:04:35 -0700

    Se realizó la segunda optimización al código

--- blackjack/assets/js/juego.js
+++ blackjack/assets/js/juego.js
@@ -12,15 +12,16 @@
           btnDetener = document.querySelector('#btnDetener'),
           btnNuevo = document.querySelector('#btnNuevo');
 
-    btnNuevo.disabled = true;
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
 
-    const divCartasJugador = document.querySelector('#jugador-cartas'),
-          divCartasComputadora = document.querySelector('#computadora-cartas'),
+    const divCartasJugadores = document.querySelectorAll('.divCartas'),
           smalls = document.querySelectorAll('.col > h1:first-of-type > small');
 
     // Esta función inicializa el juego
     const inicializarJuego = (numJugadores = 2) => {
         deck = crearDeck();
+        puntosJugadores = [];
         for(let i = 0; i < numJugadores; i++) {
             puntosJugadores.push(0);
         }
@@ -60,40 +61,44 @@
 
     const reiniciar = () => {
         inicializarJuego();
+        puntosJugadores = puntosJugadores.map(puntos => 0);
         smalls.forEach(small => small.textContent = 0);
-        [puntosJugador, puntosComputadora] = [0, 0];
-        divCartasJugador.innerText = '';
-        divCartasComputadora.innerText = '';
+        divCartasJugadores.forEach(div => div.textContent = '');
         btnPedir.disabled = false;
         btnDetener.disabled = false;
         btnNuevo.disabled = true;
     }
 
-    const acumularPuntos = () => {
+    const acumularPuntos = (carta, turno) => {
+        puntosJugadores[turno] += valorCarta(carta);
+        smalls[turno].textContent = puntosJugadores[turno];
+    }
 
+    const crearCarta = (carta, turno) => {
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasJugadores[turno].append(imgCarta);
     }
 
     // Turno de la computadora
     const turnoComputadora = (puntosMinimos) => {
         do {
             const carta = pedirCarta();
-            puntosComputadora += valorCarta(carta);
-            smalls[1].textContent = puntosComputadora;
+            acumularPuntos(carta, puntosJugadores.length - 1);
 
             //Crear y configurar la carta en memoria para asignar en el DOM
-            const imgCarta = document.createElement('img');
-            imgCarta.src = `assets/cartas/${carta}.png`;
-            imgCarta.classList.add('carta');
-            divCartasComputadora.append(imgCarta);
+            crearCarta(carta, puntosJugadores.length - 1);
             if(puntosMinimos > 21) break;
-        } while(puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+        } while(puntosJugadores[puntosJugadores.length -1] < puntosMinimos && puntosMinimos <= 21);
+        // El setTimeout sirve para que se renderizen primero las cartas antes de ganar o perder
         setTimeout(() => {
             if(puntosMinimos > 21) {
                 alert('Perdiste');
-            }else if (puntosComputadora > 21) {
+            }else if (puntosJugadores[puntosJugadores.length -1] > 21) {
                 alert('Ganaste');
             }
-            else if(puntosMinimos > puntosComputadora) {
+            else if(puntosMinimos > puntosJugadores[puntosJugadores.length -1]) {
                 alert('Ganaste');
             }
             else {
@@ -106,27 +111,23 @@
     // Eventos
     btnPedir.addEventListener('click', () => {
         const carta = pedirCarta();
-        puntosJugador += valorCarta(carta);
-        smalls[0].textContent = puntosJugador;
+        acumularPuntos(carta, 0);
 
         //Crear y configurar la carta en memoria para asignar en el DOM
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${carta}.png`;
-        imgCarta.classList.add('carta');
-        divCartasJugador.append(imgCarta);
+        crearCarta(carta, 0);
 
         // Lógica de victoria - derrota
-        if(puntosJugador >= 21) {
+        if(puntosJugadores[0] >= 21) {
             btnPedir.disabled = true;
             btnDetener.disabled = true;
-            turnoComputadora(puntosJugador);
+            turnoComputadora(puntosJugadores[0]);
         }
     });
 
     btnDetener.addEventListener('click', () => {
         btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoComputadora(puntosJugador);
+        turnoComputadora(puntosJugadores[0]);
     });
 
     btnNuevo.addEventListener('click', () => {
