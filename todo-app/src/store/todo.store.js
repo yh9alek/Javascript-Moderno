62f7755d15a4695fcc5a5b944b6032994ecfac99
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-22 10:21:54 -0700

    Se crearon las funciones que van a manejar el crud de los Todos en el store

--- todo-app/src/store/todo.store.js
+++ todo-app/src/store/todo.store.js
@@ -20,6 +20,49 @@ const initStore = () => {
     console.log('InitStore 🥑');
 }
 
-export default {
-    initStore,
+const loadStore = () => {
+    throw new Error('Not Implemented');
 }
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not Implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not Implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new Error('Not Implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not Implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not Implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not Implemented');
+}
+
+export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+};
