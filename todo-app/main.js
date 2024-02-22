97648d04efc53dbf35c846deef6f4e0f8da17659
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-22 10:09:15 -0700

    Se crearon los modelos y se creó el store del proyecto

--- todo-app/main.js
+++ todo-app/main.js
@@ -1,4 +1,6 @@
 import './style.css';
 import { App } from './src/todos/app';
+import todoStore from './src/store/todo.store';
 
+todoStore.initStore();
 App('#app');
---
+++ todo-app/src/todos/models/todo.model.js
@@ -0,0 +1,12 @@
+export class Todo {
+    /**
+     * 
+     * @param {String} description 
+     */
+    constructor(description = '') {
+        this.id = 1;
+        this.description = description;
+        this.done = false;
+        this.createdAt = new Date();
+    }
+}
