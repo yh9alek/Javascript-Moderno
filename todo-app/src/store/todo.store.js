3265b3366375085cc18b23d10d14274523e13f17
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-22 10:36:58 -0700

    Se terminó de implementar todas las funciones del store menos la de loadStore

--- todo-app/src/store/todo.store.js
+++ todo-app/src/store/todo.store.js
@@ -11,6 +11,8 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra de la realidad'),
     ],
     filter: Filters.All,
 };
@@ -51,7 +53,12 @@ const getTodos = (filter = Filters.All) => {
  * @param {String} todoId 
  */
 const toggleTodo = (todoId) => {
-    throw new Error('Not Implemented');
+    state.todos = state.todos.map(todo => {
+        if(todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
 }
 
 const deleteTodo = (todoId) => {
