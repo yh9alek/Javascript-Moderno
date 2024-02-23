ab40ba419a7832c6dcc3297e009c498d2ed985f8
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-23 10:57:30 -0700

    Se creó el archivo .env para crear variables de entorno para nuestra aplicación

--- advanced/main.js
+++ advanced/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { environmentsComponent } from './src/concepts/01-environments';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,5 +12,8 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
+    <div class='card'></div>
   </div>
 `
+const element = document.querySelector('.card');
+environmentsComponent(element);
