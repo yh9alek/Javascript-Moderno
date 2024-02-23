47186846ccd07dfdc8acc06dcfda6104ec0f5b89
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-22 17:31:50 -0700

    Se implementó la lógica para guardar los Todos que vallamos escribiendo, programando los eventos que surgan del teclado en el input principal

--- todo-app/src/todos/app.js
+++ todo-app/src/todos/app.js
@@ -4,6 +4,7 @@ import { renderTodos } from './use-cases';
 
 const ElementIDs = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 };
 
 /**
@@ -24,4 +25,14 @@ export const App = (elementId) => {
         document.querySelector(elementId).append(app);
         displayTodos();
     })();
+
+    // Referencias HTML
+    const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if(event.keyCode !== 13) return;
+        if(!event.target.value.trim().length) return;
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    });
 }
