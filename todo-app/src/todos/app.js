9bf1fe39975b58bde856b2f1d73359e355793cc5
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-02-22 19:07:11 -0700

    Se programó la lógica para contar los Todos pendientes e irlos actualizando con cada cambio en el estado global de la aplicación. Se terminó el proyecto de todo-app con Vite

--- todo-app/src/todos/app.js
+++ todo-app/src/todos/app.js
@@ -1,12 +1,13 @@
 import html from './app.html?raw';
 import todoStore, { Filters } from '../store/todo.store';
-import { renderTodos } from './use-cases';
+import { renderTodos, renderPending } from './use-cases';
 
 const ElementIDs = {
     ClearCompleted: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
     TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count',
 };
 
 /**
@@ -18,6 +19,11 @@ export const App = (elementId) => {
     const displayTodos = () => {
         const todos = todoStore.getTodos(todoStore.getCurrentFilter());
         renderTodos(ElementIDs.TodoList, todos);
+        updatePendingCount();
+    }
+
+    const updatePendingCount = () => {
+        renderPending(ElementIDs.PendingCountLabel);
     }
 
     // Cuando la función App() se llama
--- todo-app/src/todos/use-cases/index.js
+++ todo-app/src/todos/use-cases/index.js
@@ -1,2 +1,3 @@
+export { renderPending } from './render-pending';
 export { createTodoHTML } from './create-todo';
 export { renderTodos } from './render-todos';
