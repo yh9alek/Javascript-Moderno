f4accbd3b4a2a8172a41af532814988ea9fa131d
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-03-20 16:11:04 -0700

    Se creó un contador con una función generadora

--- advanced/src/concepts/09-generators.js
+++ advanced/src/concepts/09-generators.js
@@ -3,14 +3,33 @@
  * @param {HTMLDivElement} element 
  */
 export const generatorFunctionsComponent = (element) => {
-    const myGenerator = myFisrtGeneratorFunction();
+    const getId = idGenerator();
+
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append(button);
+
+    const renderValue = () => {
+        const { value } = getId.next();
+        button.innerText = `Click ${value}`;
+    }
+
+    button.addEventListener('click', renderValue);
+    /*const myGenerator = myFisrtGeneratorFunction();
 
     console.log(myGenerator.next());
     console.log(myGenerator.next());
     console.log(myGenerator.next());
     console.log(myGenerator.next());
     console.log(myGenerator.next());
-    console.log(myGenerator.next());
+    console.log(myGenerator.next());*/
+}
+
+function* idGenerator() {
+    let id = 0;
+    while(true) {
+        yield ++id;
+    }
 }
 
 function* myFisrtGeneratorFunction() {
