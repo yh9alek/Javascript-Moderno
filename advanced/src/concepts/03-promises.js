6d3d6ad705ab17620fed24e3b8bdb39b757c74b9
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-03-20 11:57:06 -0700

    Se codificaron las 3 formas de encadenar promesas

--- advanced/src/concepts/03-promises.js
+++ advanced/src/concepts/03-promises.js
@@ -10,14 +10,48 @@ export const promiseComponent = (element) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `<h3>Heroes:</h3>
+        <h4>${hero1}</h4>
+        <h4>${hero2}</h4>`;
+    }
+
     const renderError = (error) => {
         element.innerHTML = `<h3>Error:</h3>
         <h4>${error}</h4>`;
     }
 
-    findHero('5d86371f2343e37870b91ef1')
-        .then(renderHero)
+    let id1 = '5d86371f25a058e5b1c8a65e';
+    let id2 = '5d86371f9f80b591f499df32';
+
+    /* //! Promise All
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ])
+    .then(([hero1, hero2]) => renderTwoHeroes(hero1, hero2))
+    .catch(renderError);
+    */
+
+    /* //! Unión de promesas
+    findHero(id1)
+        .then(hero1 => {
+            return findHero(id2);
+        }).then(hero2 => renderTwoHeroes(hero1, hero2))
         .catch(renderError);
+    */
+
+    /* //! Promise Hell
+    findHero(id1)
+        .then(hero1 => {
+            findHero(id2)
+                .then(hero2 => {
+                    renderTwoHeroes(hero1, hero2)
+                })
+                .catch(renderError);
+        })
+        .catch(renderError);
+    */
 }
 
 /**
