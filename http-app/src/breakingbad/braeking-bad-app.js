f4e19a00eee1d3772461cc066bb55d5d9066dafc
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-03-20 17:19:42 -0700

    Se programó el fetch del quote random de breaking bad

--- http-app/src/breakingbad/braeking-bad-app.js
+++ http-app/src/breakingbad/braeking-bad-app.js
@@ -1,5 +1,14 @@
 
-export const BreakingBadApp = (element) => {
+const fetchQuote = async () => {
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    console.log(data[0]);
+    return data[0]
+}
+
+export const BreakingBadApp = async (element) => {
     document.querySelector('#app-title').textContent = 'Breaking Bad App';
     element.innerHTML = 'Loading...';
+    const quote = await fetchQuote();
+    element.innerHTML = 'Tenemos data!';
 }
