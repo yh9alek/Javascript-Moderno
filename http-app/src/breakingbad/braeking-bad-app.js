716c4bbc2d3395d48f6a4b21aaf396337bc4d1d3
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-03-20 17:32:41 -0700

    Se terminó de programar la práctica de Breaking Bad App

--- http-app/src/breakingbad/braeking-bad-app.js
+++ http-app/src/breakingbad/braeking-bad-app.js
@@ -9,6 +9,24 @@ const fetchQuote = async () => {
 export const BreakingBadApp = async (element) => {
     document.querySelector('#app-title').textContent = 'Breaking Bad App';
     element.innerHTML = 'Loading...';
-    const quote = await fetchQuote();
-    element.innerHTML = 'Tenemos data!';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (quote) => {
+        quoteLabel.innerHTML = quote.quote;
+        authoLabel.innerHTML = quote.author;
+        element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+    }
+
+    nextQuoteButton.addEventListener('click', e => {
+        element.innerHTML = 'Loading...';
+        fetchQuote()
+        .then(renderQuote);
+    });
+
+    fetchQuote()
+        .then(renderQuote);
 }
